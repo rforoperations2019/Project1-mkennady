cc99aa18ee98fd91e253accda908f653652b8028
maxkennady <50968257+maxkennady@users.noreply.github.com>
2019-09-24 14:26:31 -0400

    Create app.R
    Created app.R
    Added 3 menu items (Traffic, Analysis, and Groups Traffic)

---
+++ app.R
@@ -0,0 +1,37 @@
+library(shinydashboard)
+library(shiny)
+library(reshape2)
+library(dplyr)
+library(readr)
+library(magrittr)
+library(plotly)
+library(shinythemes)
+
+
+
+header <- dashboardHeader(title = "NAAEE Web Traffic Dashboard")
+  
+# see icons here: https://fontawesome.com/icons?d=gallery&q=bar
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Website Traffic", tabName = "dashboard", icon = icon("chart-bar"),
+             badgeLabel = "Staff", badgeColor = "light-blue"),
+    menuItem("Website Analysis", tabName = "dashboard", icon = icon("sort-numeric-up"),
+             badgeLabel = "Staff", badgeColor = "light-blue"),
+    menuItem("eePRO Groups Traffic", tabName = "widgets", icon = icon("users"), 
+             badgeLabel = "Moderators", badgeColor = "green")
+  )
+)
+
+body <- dashboardBody()
+  
+
+ui <- dashboardPage(header, sidebar, body)
+
+server <- function(input, output) { 
+  
+  
+  
+  }
+
+shinyApp(ui, server)
