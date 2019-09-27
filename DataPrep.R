72f0fd16b0967e2dfe32d2a976a978fbbcb47c4a
maxkennady <50968257+maxkennady@users.noreply.github.com>
2019-09-26 23:27:18 -0400

    Added info boxes and plot titles
    Added info boxes to pageviews and users pages; added plot titles to bar charts

--- DataPrep.R
+++ DataPrep.R
@@ -5,19 +5,19 @@ library(stringr)
 
 GAdata <- read_csv("data/2019_NAAEE_GoogleAnalytics.csv")
 GAdata$X1 <- NULL
+#GAdata$date <- as.Date(GAdata$date)
 
+GAdata$date <- as.Date(GAdata$date, format = '%m/%d/%Y')
 
 
 # dates
 # GAdata$date <- gsub('^(.{4})(.*)$', '\\1-\\2', GAdata$date)
 # GAdata$date <- gsub('^(.{7})(.*)$', '\\1-\\2', GAdata$date)
-GAdata$date <- as.Date(GAdata$date)
 
 
 GAdata$bounceRate <- as.numeric(format(round(GAdata$bounceRate,0)))
 GAdata$exitRate <- as.numeric(format(round(GAdata$exitRate,0)))
 GAdata$avgTimeOnPage <- as.numeric(format(round(GAdata$avgTimeOnPage,0)))
-
 
 write_csv(GAdata, "data/2019_NAAEE_GoogleAnalytics.csv")
 
@@ -32,10 +32,14 @@ write_csv(GAdata, "data/2019_NAAEE_GoogleAnalytics.csv")
 
 
 data <- GAdata[,colnames(GAdata) == "date"|colnames(GAdata) == "users"]
-data <- subset(data, date <= "2019-08-31" & date >= "2019-08-01")
+data <- subset(data, date <= "2019-08-01" & date >= "2019-08-01")
 
-ggplot(data, aes(x=date)) + 
-  geom_histogram()
+data <- subset(data, date == "2019-08-01")
+
+ggplot(data, aes(x=date, y=users)) + 
+  geom_bar(stat="identity")
+
+
 
 
 
--- app.R
+++ app.R
@@ -21,7 +21,7 @@ header <- dashboardHeader(title = "NAAEE Web Traffic")
 sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Pages", tabName = "pages", icon = icon("file")),
-    menuItem("Pageviews/Sessions", tabName = "pageviews", icon = icon("chart-bar")),
+    menuItem("Pageviews", tabName = "pageviews", icon = icon("chart-bar")),
     menuItem("Users", tabName = "users", icon = icon("users")),
     dateRangeInput("date", 
                    label = "Input date range",
@@ -31,13 +31,10 @@ sidebar <- dashboardSidebar(
     textInput("text",
               label = "Input URL search here",
               value = "naaee"),
-    checkboxInput(inputId = "previous",label = "Compare previous period")
+    checkboxInput(inputId = "previous",label = "Compare to previous period")
 
   )
 )
-
-
-
 
 
 body <- dashboardBody(
@@ -51,13 +48,18 @@ body <- dashboardBody(
             fluidRow(
               box(width = 6, plotOutput(outputId = "pageviews")),
               box(width = 6, conditionalPanel("input.previous", 
-                               column(width = 12, plotOutput(outputId = "pageviews.previous")))))
+                               column(width = 12, plotOutput(outputId = "pageviews.previous"))))
+              ),
+            fluidRow(infoBoxOutput("pageviewsTotal", width = 3), infoBoxOutput("pageviewsPages", width = 3)),
+            fluidRow(infoBoxOutput("pageviewsAvg"), width = 3)
     ),
 tabItem(tabName = "users",
         fluidRow(
           box(width = 6, plotOutput(outputId = "users")),
           box(width = 6, conditionalPanel("input.previous", 
-                                          column(width = 12, plotOutput(outputId = "users.previous")))))
+                                          column(width = 12, plotOutput(outputId = "users.previous"))))),
+        fluidRow(infoBoxOutput("usersTotal", width = 3), infoBoxOutput("usersPages", width = 3)),
+        fluidRow(infoBoxOutput("usersAvg"), width = 3)
 )
 )
 )
@@ -83,7 +85,7 @@ server <- function(input, output) {
   
   GA.data.previous <- reactive({
     req(input$date, input$text, input$previous)
-    date.difference <- input$date[2] - input$date[1]
+    date.difference <- input$date[2] - input$date[1] + 1
     if (input$previous) {
       subset(GA2019, GA2019$date >= input$date[1] - date.difference & 
                GA2019$date <= input$date[2] - date.difference & 
@@ -100,46 +102,81 @@ server <- function(input, output) {
     ))
   
 
-  # output$pageviews.previous <- renderPlot({
-  #   ggplot(GA.data.previous(), aes(x=date)) + 
-  #     geom_bar()
-  # })
-  # 
-  # output$pageviews <- renderPlot({
-  #   ggplot(GA.data(), aes(x=date)) + 
-  #     geom_bar()
-  # })
+  # info box ----  
+  output$pageviewsTotal <- renderInfoBox({
+    sum <- format(sum(GA.data()$pageviews, na.rm = T), big.mark = ",")
+    infoBox("Total pageviews", value = sum,
+            icon = icon("file"),
+            color = "aqua")
+  })
   
+  # info box ----  
+  output$pageviewsPages <- renderInfoBox({
+    sum <- format(nrow(GA.data()), big.mark = ",")
+    infoBox("Total pages", value = sum,
+            icon = icon("file"),
+            color = "green")
+  })
+  
+  # info box ----
+  output$pageviewsAvg <- renderInfoBox({
+    sum <- round(sum(GA.data()$pageviews, na.rm = TRUE)/nrow(GA.data()), digits = 1)
+    infoBox("Avg users per page", value = sum,
+            icon = icon("file-alt"),
+            color = "green")
+  })
+  
+  # info box ----
+  output$usersTotal <- renderInfoBox({
+    sum <- format(sum(GA.data()$users, na.rm = TRUE), big.mark = ",")
+    infoBox("Total users", value = sum,
+            icon = icon("users"),
+            color = "aqua")
+  })
+  
+  # info box ----
+  output$usersPages <- renderInfoBox({
+    sum <- format(nrow(GA.data()), big.mark = ",")
+    infoBox("Total pages", value = sum,
+            icon = icon("file"),
+            color = "green")
+  })
+  
+  # info box ----
+  output$usersAvg <- renderInfoBox({
+    sum <- round(sum(GA.data()$users, na.rm = TRUE)/nrow(GA.data()), digits = 1)
+    infoBox("Avg users per page", value = sum,
+            icon = icon("file-alt"),
+            color = "green")
+  })
   
   output$pageviews <- renderPlot({
     data <- GA.data()[,colnames(GA.data()) == "date"|colnames(GA.data()) == "pageviews"]
-    ggplot(data, aes(x=date)) + 
-      geom_histogram()
+    ggplot(data, aes(x=date, y=pageviews)) + 
+      geom_bar(stat="identity") + 
+      ggtitle(paste("Pageviews from", input$date[1], "to", input$date[2]))
   })
   
   output$pageviews.previous <- renderPlot({
     data <- GA.data.previous()[,colnames(GA.data.previous()) == "date"|colnames(GA.data.previous()) == "pageviews"]
-    ggplot(data, aes(x=date)) + 
-      geom_histogram()
+    ggplot(data, aes(x=date, y=pageviews)) + 
+      geom_bar(stat="identity") + 
+      ggtitle(paste("Pageviews from", min(GA.data.previous()$date), "to", max(GA.data.previous()$date)))
   })
-  
-  
   
   output$users <- renderPlot({
     data <- GA.data()[,colnames(GA.data()) == "date"|colnames(GA.data()) == "users"]
-    ggplot(data, aes(x=date)) + 
-      geom_histogram()
+    ggplot(data, aes(x=date, y=users)) + 
+      geom_bar(stat="identity") + 
+      ggtitle(paste("Users from", input$date[1], "to", input$date[2]))
   })
-  
   
   output$users.previous <- renderPlot({
     data <- GA.data.previous()[,colnames(GA.data.previous()) == "date"|colnames(GA.data.previous()) == "users"]
-    ggplot(data, aes(x=date)) + 
-      geom_histogram()
+    ggplot(data, aes(x=date, y=users)) + 
+      geom_bar(stat="identity") + 
+      ggtitle(paste("Users from", min(GA.data.previous()$date), "to", max(GA.data.previous()$date)))
   })
-  
-  
-  
   
   }
 
