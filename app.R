1a7c487a15713834c0d2243656d87925a4edeccc
maxkennady <50968257+maxkennady@users.noreply.github.com>
2019-09-27 14:49:26 -0400

    Added new features to graphs, infoboxes, and data table
    Added theme, background, and colors to graphs; added ggplotly to graphs; added info box on bounce rate; deleted two info boxes; added colnames and scrolling to data table; documented/commented code;

--- app.R
+++ app.R
@@ -10,8 +10,6 @@ library(shinythemes)
 library(ggplot2)
 library(DT)
 
-#https://rstudio.github.io/shinydashboard/structure.html#boxes
-
 GA2019 <- read_csv("data/2019_NAAEE_GoogleAnalytics.csv")
 
 header <- dashboardHeader(title = "NAAEE Web Traffic")
@@ -24,58 +22,41 @@ sidebar <- dashboardSidebar(
     menuItem("Pageviews", tabName = "pageviews", icon = icon("chart-bar")),
     menuItem("Users", tabName = "users", icon = icon("users")),
     dateRangeInput("date", 
-                   label = "Input date range",
+                   label = "Date range",
                    start = as.Date("2019-08-01"), end = as.Date("2019-08-31"),
                    min = as.Date("2019-01-01"), max = as.Date("2019-09-22"),
-                   format = "mm/dd/yy", startview = 'month'),
+                   format = "mm-dd-yy", startview = 'month'),
     textInput("text",
-              label = "Input URL search here",
-              value = "naaee"),
-    checkboxInput(inputId = "previous",label = "Compare to previous period")
+              label = "URL search",
+              value = "groups"),
+    checkboxInput(inputId = "previous",label = "Compare to previous period")))
 
-  )
-)
-
-
+# Reference: https://rstudio.github.io/shinydashboard/structure.html#boxes
 body <- dashboardBody(
   tabItems(
     tabItem(tabName = "pages",
-    fluidRow(
-    box(title = "Data Table", DT::dataTableOutput(outputId = "datatable"))
-    )
-),
+            fluidRow(
+              box(title = "Page Information Table", DT::dataTableOutput(outputId = "datatable"), width = 12))),
     tabItem(tabName = "pageviews",
             fluidRow(
-              box(width = 6, plotOutput(outputId = "pageviews")),
+              box(width = 6, plotlyOutput(outputId = "pageviews")),
               box(width = 6, conditionalPanel("input.previous", 
-                               column(width = 12, plotOutput(outputId = "pageviews.previous"))))
-              ),
-            fluidRow(infoBoxOutput("pageviewsTotal", width = 3), infoBoxOutput("pageviewsPages", width = 3)),
-            fluidRow(infoBoxOutput("pageviewsAvg"), width = 3)
-    ),
-tabItem(tabName = "users",
+                               column(width = 12, plotlyOutput(outputId = "pageviews.previous"))))),
+            fluidRow(infoBoxOutput("pageviewsTotal", width = 3), 
+                     infoBoxOutput("bounceAvg", width = 3))),
+    tabItem(tabName = "users",
         fluidRow(
-          box(width = 6, plotOutput(outputId = "users")),
+          box(width = 6, plotlyOutput(outputId = "users")),
           box(width = 6, conditionalPanel("input.previous", 
-                                          column(width = 12, plotOutput(outputId = "users.previous"))))),
-        fluidRow(infoBoxOutput("usersTotal", width = 3), infoBoxOutput("usersPages", width = 3)),
-        fluidRow(infoBoxOutput("usersAvg"), width = 3)
-)
-)
-)
+                                column(width = 12, plotlyOutput(outputId = "users.previous"))))),
+        fluidRow(infoBoxOutput("usersTotal", width = 3), 
+                 infoBoxOutput("usersAvg", width = 3)))))
 
-ui <- dashboardPage(header, sidebar, body)
+ui <- dashboardPage(header, sidebar, body, skin = "green")
 
 server <- function(input, output) { 
   
-  output$previousCheck <- reactive({
-    if (input$previous == FALSE) {
-      "test4"
-    }
-  })
-  
-  #outputOptions(output, "previousCheck", suspendWhenHidden = FALSE)
-  
+  #Create dataset given user's inputs
   GA.data <- reactive({
     req(input$date, input$text)
     subset(GA2019, GA2019$date >= input$date[1] & 
@@ -83,6 +64,7 @@ server <- function(input, output) {
                    (str_detect(GA2019$pagePath, tolower(input$text), negate=FALSE) == TRUE))
   })
   
+  # Create dataset for previous period
   GA.data.previous <- reactive({
     req(input$date, input$text, input$previous)
     date.difference <- input$date[2] - input$date[1] + 1
@@ -91,18 +73,19 @@ server <- function(input, output) {
                GA2019$date <= input$date[2] - date.difference & 
                (str_detect(GA2019$pagePath, tolower(input$text), negate=FALSE) == TRUE))
     }
-    
   })
   
-  
+  # Data Table ----
   output$datatable <- 
-    DT::renderDataTable(
-      DT::datatable(data = GA.data()#,
-                    # rownames = c("Date", "Page Path", "Page Title", "Users", "Pageviews", "Sessions", "Bounce Rate", "Exit Rate"))
-    ))
+    DT::renderDataTable({
+      DT::datatable(data = GA.data(), options = list(
+        autoWidth = TRUE,
+        scrollX = TRUE,
+        scrollY = 400
+      ), colnames = c("Date", "URL", "Page Title", "Users", "Pageviews", "Sessions", "Bounce Rate", "Exit Rate", "Avg Time on Page", "Entrances"))
+    })
   
-
-  # info box ----  
+  # info box for Total Pageviews ----  
   output$pageviewsTotal <- renderInfoBox({
     sum <- format(sum(GA.data()$pageviews, na.rm = T), big.mark = ",")
     infoBox("Total pageviews", value = sum,
@@ -110,23 +93,16 @@ server <- function(input, output) {
             color = "aqua")
   })
   
-  # info box ----  
-  output$pageviewsPages <- renderInfoBox({
-    sum <- format(nrow(GA.data()), big.mark = ",")
-    infoBox("Total pages", value = sum,
-            icon = icon("file"),
+  # info box for Average Bounce Rate ----
+  output$bounceAvg <- renderInfoBox({
+    avg <- round(mean(GA.data()$bounceRate), digits = 1)
+    avg <- paste(avg, "%")
+    infoBox("Average Bounce Rate", value = avg,
+            icon = icon("percent"),
             color = "green")
   })
   
-  # info box ----
-  output$pageviewsAvg <- renderInfoBox({
-    sum <- round(sum(GA.data()$pageviews, na.rm = TRUE)/nrow(GA.data()), digits = 1)
-    infoBox("Avg users per page", value = sum,
-            icon = icon("file-alt"),
-            color = "green")
-  })
-  
-  # info box ----
+  # info box for Total Users ----
   output$usersTotal <- renderInfoBox({
     sum <- format(sum(GA.data()$users, na.rm = TRUE), big.mark = ",")
     infoBox("Total users", value = sum,
@@ -134,15 +110,7 @@ server <- function(input, output) {
             color = "aqua")
   })
   
-  # info box ----
-  output$usersPages <- renderInfoBox({
-    sum <- format(nrow(GA.data()), big.mark = ",")
-    infoBox("Total pages", value = sum,
-            icon = icon("file"),
-            color = "green")
-  })
-  
-  # info box ----
+  # info box for Average Number of Users ----
   output$usersAvg <- renderInfoBox({
     sum <- round(sum(GA.data()$users, na.rm = TRUE)/nrow(GA.data()), digits = 1)
     infoBox("Avg users per page", value = sum,
@@ -150,51 +118,91 @@ server <- function(input, output) {
             color = "green")
   })
   
-  output$pageviews <- renderPlot({
+  # Pageviews graph
+  #Reference: http://www.sthda.com/english/wiki/ggplot2-themes-and-background-colors-the-3-elements
+  output$pageviews <- renderPlotly({
     data <- GA.data()[,colnames(GA.data()) == "date"|colnames(GA.data()) == "pageviews"]
-    ggplot(data, aes(x=date, y=pageviews)) + 
-      geom_bar(stat="identity") + 
-      ggtitle(paste("Pageviews from", input$date[1], "to", input$date[2]))
+    ggplotly(
+      ggplot(data, aes(x=date, y=pageviews)) + 
+        geom_bar(stat="identity", fill = "#087795", position = "identity") + 
+        labs(title = "Pageviews") +
+        theme(
+          panel.background = element_rect(fill = "white",
+                                          colour = "white",
+                                          size = 0.5, linetype = "solid"),
+          panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                          colour = "gray80"), 
+          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                          colour = "gray80"),
+          axis.title.x = element_blank()
+        ) +
+        scale_y_continuous(name = "Pageviews")
+    )
   })
   
-  output$pageviews.previous <- renderPlot({
+  # Pageviews graph for previous period
+  output$pageviews.previous <- renderPlotly({
     data <- GA.data.previous()[,colnames(GA.data.previous()) == "date"|colnames(GA.data.previous()) == "pageviews"]
-    ggplot(data, aes(x=date, y=pageviews)) + 
-      geom_bar(stat="identity") + 
-      ggtitle(paste("Pageviews from", min(GA.data.previous()$date), "to", max(GA.data.previous()$date)))
+    ggplotly(
+      ggplot(data, aes(x=date, y=pageviews)) + 
+        geom_bar(stat="identity", fill = "#087795", position = "identity") + 
+        labs(title = "Pageviews") +
+        theme(
+          panel.background = element_rect(fill = "white",
+                                          colour = "white",
+                                          size = 0.5, linetype = "solid"),
+          panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                          colour = "gray80"), 
+          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                          colour = "gray80"),
+          axis.title.x = element_blank()
+        ) +
+        scale_y_continuous(name = "Pageviews") 
+    )
   })
   
-  output$users <- renderPlot({
+  #Users graph
+  output$users <- renderPlotly({
     data <- GA.data()[,colnames(GA.data()) == "date"|colnames(GA.data()) == "users"]
-    ggplot(data, aes(x=date, y=users)) + 
-      geom_bar(stat="identity") + 
-      ggtitle(paste("Users from", input$date[1], "to", input$date[2]))
+    ggplotly(
+      ggplot(data, aes(x=date, y=users)) + 
+        geom_bar(stat="identity", fill = "#087795", position = "identity") + 
+        labs(title = "Users") +
+        theme(
+          panel.background = element_rect(fill = "white",
+                                          colour = "white",
+                                          size = 0.5, linetype = "solid"),
+          panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                          colour = "gray80"), 
+          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                          colour = "gray80"),
+          axis.title.x = element_blank() +
+            scale_y_continuous(name = "Users")
+        ) 
+    )
   })
   
-  output$users.previous <- renderPlot({
+  # Users graph for previous period
+  output$users.previous <- renderPlotly({
     data <- GA.data.previous()[,colnames(GA.data.previous()) == "date"|colnames(GA.data.previous()) == "users"]
-    ggplot(data, aes(x=date, y=users)) + 
-      geom_bar(stat="identity") + 
-      ggtitle(paste("Users from", min(GA.data.previous()$date), "to", max(GA.data.previous()$date)))
+    ggplotly(
+      ggplot(data, aes(x=date, y=users)) + 
+        geom_bar(stat="identity", fill = "#087795", position = "identity") + 
+        labs(title = "Users") +
+        theme(
+          panel.background = element_rect(fill = "white",
+                                          colour = "white",
+                                          size = 0.5, linetype = "solid"),
+          panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                          colour = "gray80"), 
+          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                          colour = "gray80"),
+          axis.title.x = element_blank()
+        ) +
+        scale_y_continuous(name = "Users")
+    )
   })
   
   }
 
 shinyApp(ui, server)
-
-
-
-
-# downloadButton(outputId = "download",
-#               label = "Download data")
-
-
-
-# output$download <- downloadHandler(
-#   filename = function() {
-#     paste('')
-#   },
-#   content = function(con) {
-#     write_csv(exports, con)
-#   }
-# )
